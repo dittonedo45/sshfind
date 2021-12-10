41d65b68eca9f88c9e1c7b1a88cd588c35ca8365
Eddington Chamunorwa <dittonedo45@github.com>
2021-12-10 07:57:00 +0200

    Test Compilation.

---
+++ CMakeFiles/sshfind.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sshfind.dir/main.c.o"
+  "sshfind"
+  "sshfind.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/sshfind.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
